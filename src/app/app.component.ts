8f0bcd90c8bd1f1f1106c8a84d2b331e3b8492a9
Dnyanashree <rohit@macbookpro.speedport.ip>
2025-03-23 16:38:53 +0100

    Pushing frontend changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from "./main-page/main-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, MainPageComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pune-traffic-app';
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [NavBarComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss'
+})
+export class MainPageComponent {
+
+}
